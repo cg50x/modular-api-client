f692ed49140e1022a301dbdfa2d1b8485da1ce1c
Chris Guevara <chris@homdna.com>
2017-09-15 13:49:31 -0700

    Add core ads and user services

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-import { createAPIClient } from './api-client';
+import { createAPIClient, addService } from './api-client';
+import { CoreAPIService } from './core-api-service';
+import { UserAPIService } from './user-api-service';
+import { AdsAPIService } from './ads-api-service';
 
 export const client = createAPIClient();
+
+addService('core', () => {
+    return new CoreAPIService();
+});
+
+addService('ads', () => {
+    return new AdsAPIService();
+});
+
+addService('user', () => {
+    return new UserAPIService();
+});
